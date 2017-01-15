1f0c873a0de7408607e112bcc42c68f66b739f37
postolenko <denis.postolenko@yandex.ua>
2017-01-15 13:25:43 +0200

    FoxtrotMebel

---
+++ js/good_slider.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+	$(function() {
+
+		var bigPhotoPath = "good_slider/big_photos/";
+		var previewPhotoPath = "good_slider/preview/";
+
+
+		$(".miniature-box").click( function() {
+
+			var miniatureIndex = $(".miniature-box").index(this);
+
+			var srcVal = $(".miniature-box:eq("+ miniatureIndex +") img").attr("src");
+
+			var indexOfSlash = srcVal.lastIndexOf("/");
+
+			var nameFile = srcVal.slice(indexOfSlash + 1);
+
+			console.log(nameFile);
+
+			$(".big_photo").css({"opacity" : .3});
+
+			$(".big_photo").attr("src", bigPhotoPath + nameFile);
+
+			$(".big_photo").css({"opacity" : 1});
+
+		});
+
+
+		$(".zoom-in").click(function() {
+
+			$(".preview-slide").fadeIn(300);
+
+			var bigPhotoSrc = $(".big_photo").attr("src");
+
+			var indexOfSlashBigPhoto = bigPhotoSrc.lastIndexOf("/");
+
+			var nameFileBigPhoto = bigPhotoSrc.slice(indexOfSlashBigPhoto + 1);
+
+			$(".preview-img").attr("src", previewPhotoPath + nameFileBigPhoto);
+
+			console.log(nameFileBigPhoto);
+
+			console.log($(".preview-img"));
+
+		});
+
+		$(".preview-bg, .zoom-out").click(function() {
+
+			$(".preview-slide").fadeOut(300);
+
+		});
+
+
+
+	});
+
+});
